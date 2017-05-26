94e960b95e7f6ec1946f343485baec20d02ee913
palzuncoff <palzuncoff@yahoo.com>
2017-05-26 17:49:06 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+//session
+var session = require('express-session'); // Сессии
+var MongoStore = require('connect-mongo')(session); // Хранилище сессий в монгодб
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+require('./bin/dbinit');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+//session
+app.use(session({
+  secret: 'Химера Хирера',
+  // Замените на что нибудь
+  resave: false,
+  // Пересохранять даже если нету изменений
+  saveUninitialized: true,
+  // Сохранять пустые сессии
+  store: new MongoStore({ mongooseConnection: require('mongoose').connection })
+  // Использовать монго хранилище
+}));
+
+
+app.use('/', index);
+app.use('/users', users);
+
+// Обработчик ошибок
+app.use(require('./error/errorHandler'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ models/gift.js
@@ -0,0 +1,51 @@
+/**
+ * Created by palzuncoff on 5/26/17.
+ */
+/**
+ * Created by palzuncoff on 5/22/17.
+ */
+var mongoose = require('lib/mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+  name: {
+    type: String,
+    maxlength:[100,"tooLong"],
+    minlength:[2,"tooShort"],
+    required:[true,"giftNameRequired"]
+  },
+  price: {
+    type: Number,
+    required:[true,"giftPriceRequired"]
+  },
+  end_offer_date: {
+    type: Date
+  },
+  is_offer_avalabale: {
+    type: Boolean,
+    default: true
+  },
+  is_delivery: {
+    type: Boolean,
+    default: false
+  },
+  is_hot_offer: {
+    type: Boolean,
+    default: false
+  },
+  pub_date: {
+    type: Date,
+    default: Date.now
+  },
+  discount: {
+    type: Number
+  },
+  preview: {
+    data: Buffer,
+    contentType: String
+  }
+});
+
+module.exports = mongoose.model('Gift', schema);
+
+
