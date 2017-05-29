6a11d7e18747e082e66e20dd2918bdd416228d77
palzuncoff <palzuncoff@yahoo.com>
2017-05-30 00:23:54 +0300

    add logger

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+var Logger = require('./logger');
+var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+//logger
+var logger = new Logger();
 
 //session
 var session = require('express-session'); // Сессии
@@ -22,7 +26,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
